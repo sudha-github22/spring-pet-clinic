600fac7c56fdfe3b8bc8d85fd9a9e6ed05617648
sudha-github22 <sudha.jan90@gmail.com>
2022-06-05 23:25:31 +0100

    Updated Visit to JPA entity -closes#39

--- pet-clinic-data/src/main/java/com/ssd/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ssd/springpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.ssd.springpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="pets")
@@ -16,6 +18,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name="birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -47,5 +52,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
