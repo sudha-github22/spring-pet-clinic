4d99a717ef7d0133dc3fbc469e1513abc9483f79
sudha-github22 <sudha.jan90@gmail.com>
2022-06-06 15:24:48 +0100

    Created Spring Data JPA Owner service -closes#41

--- pet-clinic-data/src/main/java/com/ssd/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/ssd/springpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.ssd.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+    public Owner findByLastName(String lastName);
 }
