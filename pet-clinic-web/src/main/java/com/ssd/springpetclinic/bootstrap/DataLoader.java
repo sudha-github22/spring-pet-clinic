f16d341dad1aaec061874f548b558d91439cd403
sudha-github22 <sudha.jan90@gmail.com>
2022-06-06 18:24:10 +0100

    Implemented Spring Data JPA Visit service -closes#47

--- pet-clinic-web/src/main/java/com/ssd/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ssd/springpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.ssd.springpetclinic.bootstrap;
 
 import com.ssd.springpetclinic.model.*;
-import com.ssd.springpetclinic.services.OwnerService;
-import com.ssd.springpetclinic.services.PetTypeService;
-import com.ssd.springpetclinic.services.SpecialityService;
-import com.ssd.springpetclinic.services.VetService;
+import com.ssd.springpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +13,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService,PetTypeService petTypeService,SpecialityService specialityService){
+    public DataLoader(OwnerService ownerService, VetService vetService,PetTypeService petTypeService,
+                      SpecialityService specialityService,VisitService visitService){
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -73,14 +73,20 @@ public class DataLoader implements CommandLineRunner {
         owner2.setCity("Dublin");
         owner2.setTelephone("45565709821");
 
-        Pet robertsPet = new Pet();
-        robertsPet.setName("Meow");
-        robertsPet.setPetType(savedCatType);
-        robertsPet.setBirthDate(LocalDate.now());
-        robertsPet.setOwner(owner2);
-        owner2.getPets().add(robertsPet);
+        Pet robertsCat = new Pet();
+        robertsCat.setName("Meow");
+        robertsCat.setPetType(savedCatType);
+        robertsCat.setBirthDate(LocalDate.now());
+        robertsCat.setOwner(owner2);
+        owner2.getPets().add(robertsCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(robertsCat);
+        catVisit.setDescription("sneezy kitty ");
+        catVisit.setDate(LocalDate.now());
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owner Data......");
 
